6883836d4d07d0b0d80a09fa11646d5994cb19e0
Peharge <Julian.kropff@gmx.de>
2025-04-24 12:42:05 +0200

    Update pp-term.py

--- pp-term/pp-term.py
+++ pp-term/pp-term.py
@@ -562,7 +562,7 @@ def is_tool_installed(tool_name):
 
 
 def setup_autocomplete():
-    commands = ["cd", "cls", "clear", "dir", "ls", "mkdir", "rmdir", "del", "rm", "echo", "type", "cat", "exit", "lx", "m", "mp", "ms",
+    commands = ["cd", "cls", "clear", "dir", "ls", "mkdir", "rmdir", "del", "rm", "echo", "type", "cat", "exit", "lx", "p", "pp", "ps",
                 "ubuntu", "debian", "kali", "hack", "arch", "opensuse", "mint", "fedora", "redhat", "sles", "pengwin", "oracle", "clear",
                 "alpine", "etc."]
     readline.set_completer(lambda text, state: [cmd for cmd in commands if cmd.startswith(text)][state] if state < len(
