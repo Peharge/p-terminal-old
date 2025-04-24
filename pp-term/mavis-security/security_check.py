61575b0fca4d65c1d23a3f55bb30c52d7fd79a5c
Peharge <Julian.kropff@gmx.de>
2025-04-24 11:53:05 +0200

    pp-term -> mavis-security

---
+++ pp-term/mavis-security/security_check.py
@@ -0,0 +1,115 @@
+# Englisch | Peharge: This source code is released under the MIT License.
+#
+# Usage Rights:
+# The source code may be copied, modified, and adapted to individual requirements.
+# Users are permitted to use this code in their own projects, both for private and commercial purposes.
+# However, it is recommended to modify the code only if you have sufficient programming knowledge,
+# as changes could cause unintended errors or security risks.
+#
+# Dependencies and Additional Frameworks:
+# The code relies on the use of various frameworks and executes additional files.
+# Some of these files may automatically install further dependencies required for functionality.
+# It is strongly recommended to perform installation and configuration in an isolated environment
+# (e.g., a virtual environment) to avoid potential conflicts with existing software installations.
+#
+# Disclaimer:
+# Use of the code is entirely at your own risk.
+# Peharge assumes no liability for damages, data loss, system errors, or other issues
+# that may arise directly or indirectly from the use, modification, or redistribution of the code.
+#
+# Please read the full terms of the MIT License to familiarize yourself with your rights and obligations.
+
+# Deutsch | Peharge: Dieser Quellcode wird unter der MIT-Lizenz veröffentlicht.
+#
+# Nutzungsrechte:
+# Der Quellcode darf kopiert, bearbeitet und an individuelle Anforderungen angepasst werden.
+# Nutzer sind berechtigt, diesen Code in eigenen Projekten zu verwenden, sowohl für private als auch kommerzielle Zwecke.
+# Es wird jedoch empfohlen, den Code nur dann anzupassen, wenn Sie über ausreichende Programmierkenntnisse verfügen,
+# da Änderungen unbeabsichtigte Fehler oder Sicherheitsrisiken verursachen könnten.
+#
+# Abhängigkeiten und zusätzliche Frameworks:
+# Der Code basiert auf der Nutzung verschiedener Frameworks und führt zusätzliche Dateien aus.
+# Einige dieser Dateien könnten automatisch weitere Abhängigkeiten installieren, die für die Funktionalität erforderlich sind.
+# Es wird dringend empfohlen, die Installation und Konfiguration in einer isolierten Umgebung (z. B. einer virtuellen Umgebung) durchzuführen,
+# um mögliche Konflikte mit bestehenden Softwareinstallationen zu vermeiden.
+#
+# Haftungsausschluss:
+# Die Nutzung des Codes erfolgt vollständig auf eigene Verantwortung.
+# Peharge übernimmt keinerlei Haftung für Schäden, Datenverluste, Systemfehler oder andere Probleme,
+# die direkt oder indirekt durch die Nutzung, Modifikation oder Weitergabe des Codes entstehen könnten.
+#
+# Bitte lesen Sie die vollständigen Lizenzbedingungen der MIT-Lizenz, um sich mit Ihren Rechten und Pflichten vertraut zu machen.
+
+# Français | Peharge: Ce code source est publié sous la licence MIT.
+#
+# Droits d'utilisation:
+# Le code source peut être copié, édité et adapté aux besoins individuels.
+# Les utilisateurs sont autorisés à utiliser ce code dans leurs propres projets, à des fins privées et commerciales.
+# Il est cependant recommandé d'adapter le code uniquement si vous avez des connaissances suffisantes en programmation,
+# car les modifications pourraient provoquer des erreurs involontaires ou des risques de sécurité.
+#
+# Dépendances et frameworks supplémentaires:
+# Le code est basé sur l'utilisation de différents frameworks et exécute des fichiers supplémentaires.
+# Certains de ces fichiers peuvent installer automatiquement des dépendances supplémentaires requises pour la fonctionnalité.
+# Il est fortement recommandé d'effectuer l'installation et la configuration dans un environnement isolé (par exemple un environnement virtuel),
+# pour éviter d'éventuels conflits avec les installations de logiciels existantes.
+#
+# Clause de non-responsabilité:
+# L'utilisation du code est entièrement à vos propres risques.
+# Peharge n'assume aucune responsabilité pour tout dommage, perte de données, erreurs système ou autres problèmes,
+# pouvant découler directement ou indirectement de l'utilisation, de la modification ou de la diffusion du code.
+#
+# Veuillez lire l'intégralité des termes et conditions de la licence MIT pour vous familiariser avec vos droits et responsabilités.
+
+import subprocess
+import sys
+
+# Farbcodes definieren
+red = "\033[91m"
+green = "\033[92m"
+yellow = "\033[93m"
+blue = "\033[94m"
+magenta = "\033[95m"
+cyan = "\033[96m"
+white = "\033[97m"
+black = "\033[30m"
+orange = "\033[38;5;214m"
+reset = "\033[0m"
+bold = "\033[1m"
+
+def get_user_input():
+    try:
+        print("\nSecurity check Information:")
+        print("---------------------------")
+        user_input = input(f"Do you want to perform a security check? [y/n]:").strip().lower()
+        return user_input
+    except (EOFError, KeyboardInterrupt):
+        print(f"{red}{bold}\nInput interrupted. Exiting the program.{reset}")
+        sys.exit(1)
+
+def execute_installation():
+    try:
+        subprocess.run([sys.executable, "security/run-security-check.py"], check=True)
+        print(f"{blue}{bold}MAVIS Security check completed successfully.{reset}")
+    except subprocess.CalledProcessError:
+        print(f"{red}{bold}An error occurred while running the Security check script.{reset}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"{red}{bold}Security check skipped...{reset}")
+        sys.exit(1)
+
+def main():
+    while True:
+        user_input = get_user_input()
+
+        if user_input in ["y", "yes"]:
+            execute_installation()
+            break
+        elif user_input in ["n", "no"]:
+            print(f"{blue}{bold}Security check was declined. Exiting the program.\n{reset}")
+            sys.exit(0)
+        else:
+            print(f"{red}{bold}Invalid input. Please enter 'y/yes' or 'n/no'.{reset}")
+
+if __name__ == "__main__":
+    main()
