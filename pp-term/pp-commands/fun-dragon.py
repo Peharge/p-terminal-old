b322b45db16040135c38caddaea88118acaa418f
Peharge <Julian.kropff@gmx.de>
2025-04-26 16:45:43 +0200

    update pp-term -> fun

--- pp-term/pp-commands/fun-dragon.py
+++ pp-term/pp-commands/fun-dragon.py
@@ -84,4 +84,4 @@ def run_alpine_python_command(command):
 
 
 # Beispielbefehl
-run_alpine_python_command('cowsay -f dragon "I am a dragon!"')
+run_alpine_python_command('cowsay -f dragon "Hello, I am a dragon!"')
