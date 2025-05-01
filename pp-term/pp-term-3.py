b03367931254187ac2560dccc6139b2956b5a900
Peharge <Julian.kropff@gmx.de>
2025-05-01 20:10:00 +0200

    update pp-term -> pp-term-3.py

--- pp-term/pp-term-3.py
+++ pp-term/pp-term-3.py
@@ -561,10 +561,12 @@ def handle_special_commands(user_input):
         "p pip ls": "pp-commands\\p-python-frameworks.py",  # new
         "p git ls": "pp-commands\\p-git.py", # new
         "p git": "pp-commands\\p-git.py",  # new
+        "p git p-terminal": "pp-commands\\p-git.py",  # new
         "p git mavis": "pp-commands\\p-git-mavis.py",  # new
         "p git mavis-web": "pp-commands\\p-git-mavis-web.py",  # new
         "p git simon": "pp-commands\\p-git-simon.py",  # new
         "p ls": "pp-commands\\p-ls.py", # new
+        "p p-terminal ls": "pp-commands\\p-ls.py",  # new
         "p ls mavis": "pp-commands\\p-ls-mavis.py",  # new
         "p ls mavis-web": "pp-commands\\p-ls-mavis-web.py",  # new
         "p ls simon": "pp-commands\\p-ls-simon.py",  # new
