cb7769be04187be3a5e15cf9eb70ff8be5977b63
Peharge <Julian.kropff@gmx.de>
2025-05-04 13:06:34 +0200

    update pp-term -> pp-term-3.py

--- pp-term/pp-term-3.py
+++ pp-term/pp-term-3.py
@@ -1907,6 +1907,27 @@ COLOR_SCHEMES = {
         "red": "#E92888",
         "white": "#EAEAEA",
         "yellow": "#CE9178"
+    },
+    "alternative": {
+        "name": "alternative",
+        "black": "#101116",
+        "red": "#ff5680",
+        "green": "#00ff9c",
+        "yellow": "#fffc58",
+        "blue": "#00b0ff",
+        "purple": "#d57bff",
+        "cyan": "#76c1ff",
+        "white": "#c7c7c7",
+        "brightBlack": "#686868",
+        "brightRed": "#ff6e67",
+        "brightGreen": "#5ffa68",
+        "brightYellow": "#fffc67",
+        "brightBlue": "#6871ff",
+        "brightPurple": "#d682ec",
+        "brightCyan": "#60fdff",
+        "brightWhite": "#ffffff",
+        "background": "#1d2342",
+        "foreground": "#b8ffe1"
     }
 }
 
