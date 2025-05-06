b205d4ed8067abc8efc756cb0c8d238c51952af6
Peharge <Julian.kropff@gmx.de>
2025-05-06 16:48:32 +0200

    update pp-term -> ...

--- pp-term/peharge-cpp-compier/build_peharge.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-echo Visual Studio Compiler Environment enabled.
-cd /d "%~dp0\..\..\peharge"
-echo Start compiling the peharge project...
-cl.exe /EHsc main.cpp /Fe:peharge_project.exe
-pause
