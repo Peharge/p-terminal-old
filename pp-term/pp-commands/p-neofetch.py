f89d44e56e6c5ece0500bf603b90c0b3eb6e5bc3
Peharge <Julian.kropff@gmx.de>
2025-05-07 13:23:36 +0200

    update pp-term -> p-neofetch

--- pp-term/pp-commands/p-neofetch.py
+++ pp-term/pp-commands/p-neofetch.py
@@ -127,10 +127,10 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 # p-terminal Static Versions
-P_Terminal_VERSION = "1.1"
-PP_Terminal_VERSION = "3"
-Peharge_C_COMP_VERSION = "3"
-Peharge_CPP_COMP_VERSION = "3"
+P_Terminal_VERSION = "1"
+PP_Terminal_VERSION = "4"
+Peharge_C_COMP_VERSION = "4"
+Peharge_CPP_COMP_VERSION = "4"
 P_Terminal_License = "MIT"
 
 # Inline QSS Stylesheet
