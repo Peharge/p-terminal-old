566175886e9bca5413e3f6a18151c0c7ac13f61a
Peharge <Julian.kropff@gmx.de>
2025-05-08 19:34:45 +0200

    update pp-term -> ...

--- pp-term/pp-commands/run-jup.py
+++ pp-term/pp-commands/run-jup.py
@@ -64,19 +64,13 @@
 import subprocess
 import sys
 import os
+from datetime import datetime
 
-# Farbcodes definieren
-red = "\033[91m"
-green = "\033[92m"
-yellow = "\033[93m"
-blue = "\033[94m"
-magenta = "\033[95m"
-cyan = "\033[96m"
-white = "\033[97m"
-black = "\033[30m"
-orange = "\033[38;5;214m"
-reset = "\033[0m"
-bold = "\033[1m"
+def timestamp() -> str:
+    """Returns current time formatted with milliseconds"""
+    now = datetime.now()
+    return now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
 
 def start_jupyter():
     try:
@@ -87,18 +81,18 @@ def start_jupyter():
         venv_python = os.path.join(user_home, "p-terminal", "pp-term", ".env", "Scripts", "python.exe")
 
         if not os.path.isfile(venv_python):
-            print(f"{red}Error{reset}: The virtual environment {venv_python} does not exist.")
+            print(f"[{timestamp()}] [ERROR] The virtual environment {venv_python} does not exist.")
             sys.exit(1)
 
         try:
             subprocess.run([venv_python, "-m", "pip", "show", "jupyter"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         except subprocess.CalledProcessError:
-            print(f"{red}Error: Jupyter is not installed in the virtual environment.{reset}")
+            print(f"[{timestamp()}] [ERROR] Jupyter is not installed in the virtual environment.")
             sys.exit(1)
 
-        jupyter_directory = os.path.join(user_home, "p-terminal", "pp-term", "jupyter")
+        jupyter_directory = os.path.join(user_home, "p-terminal", "pp-term", "mavis-jupyter")
         if not os.path.isdir(jupyter_directory):
-            print(f"{red}Error: The specified directory '{jupyter_directory}' does not exist.{reset}")
+            print(f"[{timestamp()}] [ERROR] The specified directory '{jupyter_directory}' does not exist.")
             sys.exit(1)
 
         # Starten von Jupyter ohne Token und Passwort, mit verbesserten CORS- und Sicherheitskonfigurationen
@@ -122,14 +116,14 @@ def start_jupyter():
         try:
             subprocess.run(jupyter_command, check=True)
         except subprocess.CalledProcessError as e:
-            print(f"{yellow}Restarting Jupyter due to error:{reset} {e}")
+            print(f"[{timestamp()}] [INFO] Restarting Jupyter due to error: {e}")
             subprocess.run(jupyter_command, check=True)
 
     except subprocess.CalledProcessError as e:
-        print(f"{red}Error starting Jupyter Notebook{reset}: {e}")
+        print(f"[{timestamp()}] [ERROR] Error starting Jupyter Notebook: {e}")
         sys.exit(1)
     except Exception as e:
-        print(f"{red}Unexpected error{reset}: {e}")
+        print(f"[{timestamp()}] [ERROR] Unexpected error: {e}")
         sys.exit(1)
 
 if __name__ == "__main__":
