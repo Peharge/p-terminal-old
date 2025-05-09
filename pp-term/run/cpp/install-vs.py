3eb0891a20a4664c34982af176d89405e17eb25b
Peharge <Julian.kropff@gmx.de>
2025-05-09 11:43:48 +0200

    update pp-term -> ...

--- pp-term/run/cpp/install-vs.py
+++ pp-term/run/cpp/install-vs.py
@@ -82,7 +82,7 @@ import logging
 # Logging-Konfiguration
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
+    format="[%(asctime)s] [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     stream=sys.stdout
 )
