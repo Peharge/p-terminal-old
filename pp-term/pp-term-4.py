fe9ce6a927ab8144723a5e43d42d4a44881c9d37
Peharge <Julian.kropff@gmx.de>
2025-05-09 08:19:34 +0200

    update pp-term -> pp-term-4.py

--- pp-term/pp-term-4.py
+++ pp-term/pp-term-4.py
@@ -112,7 +112,7 @@ current_theme = "dark"
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S.%f",
+    datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[logging.StreamHandler()]
 )
 
